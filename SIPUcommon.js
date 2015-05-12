359bf9782d07c903904f0fd78425c232ad8e8d3d
SIPU <ddasik00@naver.com>
2015-05-12 17:49:51 +0900

    Update SIPUcommon.js
    event listener add

--- SIPUcommon.js
+++ SIPUcommon.js
@@ -1,106 +1,101 @@
 var SIPUcommon = (function(SIPUcommon, $, undefined) {
-	var Set = {
-		Value : {
-
-		} 
-		,Node : {
-
-		}
-		,DataSet : {
-
-		}
-	};
 	Object.prototype.getType = function () {
-		var Obj = {
+		var ObjType = {
 			Value : ""
 			,Type : ""
 			,StructValue : ""
 		};	
 
-		Obj.Type = this.constructor.name;
-		Obj.Value = this.valueOf();
-		Obj.StructValue = this;
-		if (Obj.Type === "Function") {
-			Obj.Value = this.apply();
+		ObjType.Type = this.constructor.name;
+		ObjType.Value = this.valueOf();
+		ObjType.StructValue = this;
+		if (ObjType.Type === "Function") {
+			ObjType.Value = this.apply();
 		}
 		if (arguments[0] === "log") {
-			console.log(Obj.Type);
-			console.log(Obj.Value);
-			console.log(Obj.StructValue);
+			console.log(ObjType.Type);
+			console.log(ObjType.Value);
+			console.log(ObjType.StructValue);
 			console.log();
 		}
-		return Obj;
-	}
-	Object.prototype.getNode = function () {
-		var Obj = {
+		return ObjType;
+	};
+	
+	String.prototype.getNode = function () {
+		var ObjNode = {
 			CssName : ""
 			,NodeName : ""
 		};	
 		if (this.constructor.name === "String") {
 			if (this.valueOf().split("#").length > 1 || this.valueOf().split(".").length > 1 ) {
-				Obj.NodeName = this.valueOf().replace("#","").replace("."," ");
-				if (Obj.NodeName[0] === " ") { Obj.NodeName[0] = ""; }
-				Obj.NodeName = Obj.NodeName.concat();
-				Obj.CssName = this.valueOf();
+				ObjNode.NodeName = this.valueOf().replace("#","").replace("."," ");
+				if (ObjNode.NodeName[0] === " ") { ObjNode.NodeName[0] = ""; }
+				ObjNode.NodeName = ObjNode.NodeName.concat();
+				ObjNode.CssName = this.valueOf();
 			} else {
-				Obj.NodeName = this.valueOf();
+				ObjNode.NodeName = this.valueOf();
 				console.log();
 				if ($(".".concat(this.replace(" ","."))).length > 0) {
-					Obj.CssName = ".".concat(this.replace(" ","."));
+					ObjNode.CssName = ".".concat(this.replace(" ","."));
 				}
 				if ($("#".concat(this)).length > 0) {
-					Obj.CssName = "#".concat(this);
+					ObjNode.CssName = "#".concat(this);
 				}
 			}
 		} else {
-			Obj = this.valueOf();
+			ObjNode = this.valueOf();
 			console.log("not Node!");
-			console.log(Obj);
+			console.log(ObjNode);
 		}
-		return Obj;
+		return ObjNode;
 	};
-	Object.prototype.getTag = function () {
-		var Obj = {
-			TagName : ""
-		};	
-		if (this.constructor.name === "String") {
-			if (this.valueOf().split("#").length > 1 || this.valueOf().split(".").length > 1 ) {
-				Obj.NodeName = this.valueOf().replace("#","").replace("."," ");
-				if (Obj.NodeName[0] === " ") { Obj.NodeName[0] = ""; }
-				Obj.NodeName = Obj.NodeName.concat();
-				Obj.CssName = this.valueOf();
-			} else {
-				Obj.NodeName = this.valueOf();
-				console.log();
-				if ($(".".concat(this.replace(" ","."))).length > 0) {
-					Obj.CssName = ".".concat(this.replace(" ","."));
-				}
-				if ($("#".concat(this)).length > 0) {
-					Obj.CssName = "#".concat(this);
-				}
-			}
-		} else {
-			Obj = this.valueOf();
-			console.log("not Node!");
-			console.log(Obj);
+	
+	String.prototype.getTag = function () {
+		var ObjTag;
+		var nodeObj = this.getNode();
+
+		if  (nodeObj.CssName.split("#").length > 1 ) {
+			var node = document.getElementById(this.getNode().NodeName);
+			ObjTag = node.tagName;
+		}		
+		if  ((this.getNode().CssName).split(".").length > 1 ) {
+			ObjTag = document.getElementsByClassName(this.getNode().NodeName)[0].tagName;
+		}		
+		if (arguments[0] === "log") {
+			console.log(ObjTag);
 		}
-		if ( $(Obj.CssName).length < 1 ) {
-			Obj = this.valueOf();
-			console.log("not Node!");
-			console.log(Obj);
-		}
-		return Obj;
+		return ObjTag;
 	};
-	function getNodeList (AttrName) {
-		var temp = [];
-		var node = document.body.getElementsByTagName("*");
-		$(node).each(function (index) {
-			if (node[index].hasAttribute(AttrName)) {
-				temp.push(node[index]);
-			}
+	
+	String.prototype.getTrigger = function () {
+		var ObjTrigger = {}, node, event, proto, logging = false;
+		var nodeObj = this.getNode();
+		if (arguments[0] === "log") {logging = true;};
+		if  (nodeObj.CssName.split("#").length > 1 ) {
+			node = document.getElementById(this.getNode().NodeName);
+			event = $._data(node,"events");
+		}		
+		if  ((this.getNode().CssName).split(".").length > 1 ) {
+			node = document.getElementsByClassName(this.getNode().NodeName)[0];
+			event = $._data(node,"events");
+		}		
+		$.each(event, function (i, ev) {
+			$.each(ev, function (j, e) {
+				ObjTrigger[i] = [];
+				if (logging) {
+					console.log(i);
+				}
+				if  (e.handler != undefined && e.handler.toString().length > 0 ) {
+					ObjTrigger[i].push(e.handler.toString());
+					if (logging) {
+						console.log(e.handler);
+					}
+				}
+			});
 		});
-		return temp;
-	}
+		console.log(ObjTrigger);
+		return ObjTrigger;
+	};
 
 
     SIPUcommon.run = function() {
